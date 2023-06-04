c00f3ca7a882f13637240202bca2a40ae5ad34e1
Leonardo Gonzalez Salas <119532306+LeoAgs5001@users.noreply.github.com>
2023-06-04 10:12:18 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ function chillHop() {
             cover: "https://chillhop.com/wp-content/uploads/2021/03/75adfe0661d06a9ea66d9c2e99b31e92ae450ebe-1024x1024.jpg",
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
-            audio: "https://mp3.chillhop.com/serve.php/?mp3=16076",
+            audio: "https://prod-44-202-137-138.wostreaming.net/audacy-weeifmaac-imc",
             active: true,
         },
         {
